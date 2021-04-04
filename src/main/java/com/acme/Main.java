b9dc76002bc13922b0e69acb34940df90bdf2249
Andres Almiray <aalmiray@gmail.com>
2021-04-04 19:14:44 +0200

    Add sources

---
+++ src/main/java/com/acme/Main.java
@@ -0,0 +1,14 @@
+package com.acme;
+
+/**
+ * Do stuff
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().getValue());
+    }
+
+    public String getValue() {
+        return "42";
+    }
+}
